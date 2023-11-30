4c60b0775b43e6fb9470eb88994111f1e8586415
Johnny Domino <jvdomino@gmail.com>
2023-11-30 18:00:00 -0500

    remove preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from 'astro/config'
-import preact from '@astrojs/preact'
 import tailwind from '@astrojs/tailwind'
 import lit from '@astrojs/lit'
 
 // https://astro.build/config
 export default defineConfig({
-    // Enable Preact to support Preact JSX components.
-    integrations: [preact({ include: ['**/*[jt]sx'] }), tailwind(), lit()],
+    integrations: [tailwind(), lit()],
     server: {
         host: '0.0.0.0',
     },
