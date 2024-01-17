73d65fe4209848665ccdc1538484704474fa5b14
Johnny Domino <jvdomino@gmail.com>
2024-01-17 14:45:19 -0500

    fix padding when editing cells

--- src/components/table/td.ts
+++ src/components/table/td.ts
@@ -120,7 +120,7 @@ export class TableData extends MutableElement {
               html`&nbsp;<input .value=${this.value} @input=${this.onChange} @keydown=${this.onKeyDown} class=${classMap({
                   'z-10 absolute top-0 bottom-0 right-0 left-0': true,
                   'bg-blue-50 dark:bg-blue-950 outline-none focus:ring-2 focus:ring-blue-300 dark:focus:ring-blue-700': true,
-                  'font-mono px-2': true,
+                  'font-mono px-3': true,
               })} @blur=${this.onBlur}></input>`
             : this.blank
               ? html`<slot></slot>`
