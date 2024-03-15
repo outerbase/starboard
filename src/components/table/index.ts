2f96258d002e09f7c3912364ea8e957e782b9c2a
Johnny Domino <jvdomino@gmail.com>
2024-03-15 15:34:03 -0400

    fix erroneous start/end indices

--- src/components/table/index.ts
+++ src/components/table/index.ts
@@ -304,13 +304,12 @@ export class Table extends ClassifiedElement {
 
     public override connectedCallback(): void {
         super.connectedCallback()
-
-        // wait for the DOM to contain the #scroller
         setTimeout(() => {
-            // then add PASSIVE event listeners
-            const scroller = this.shadowRoot?.querySelector('#scroller')
-            scroller?.addEventListener('scroll', this.onScroll, { passive: true })
-            scroller?.addEventListener('scrollend', this.onScroll, { passive: true })
+            this.scroller = this.shadowRoot?.querySelector('#scroller')
+            this.scrollContainer = this.shadowRoot!.querySelector('.scroll-container') as HTMLElement | undefined
+
+            this.scroller?.addEventListener('scroll', this.onScroll, { passive: true })
+            if (!IS_SAFARI) this.scroller?.addEventListener('scrollend', this.onScroll, { passive: true })
         }, 0)
     }
 
